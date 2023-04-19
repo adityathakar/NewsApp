7f8df20db046c1479d9f7f785a6eb0a5e044b240
Adi <aditya@myzeller.com>
2023-04-19 23:17:53 +1000

    Sources UI

--- app/src/main/java/com/example/newsapp/ui/screens/sources/SourcesScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/sources/SourcesScreen.kt
@@ -1,38 +1,72 @@
 package com.example.newsapp.ui.screens.sources
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.example.newsapp.R
 
 @Composable
 fun SourcesScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.teal_700))
-            .wrapContentSize(Alignment.Center)
+    SourcesList(sources = items)
+}
+
+@Composable
+fun SourcesList(sources: List<SourceItemModel>) {
+    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+        items(items = sources) {
+            SourceItem(sourceItemModel = it)
+        }
+    }
+}
+
+@Composable
+fun SourceItem(sourceItemModel: SourceItemModel) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = "Sources",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+            text = sourceItemModel.sourceTitle,
+            fontSize = 12.sp, fontWeight = FontWeight.Bold
+        )
+        Checkbox(
+            checked = sourceItemModel.isChecked,
+            onCheckedChange = {}
         )
     }
+}
+
+data class SourceItemModel(
+    val sourceTitle: String,
+    val isChecked: Boolean,
+)
+
+val items = listOf(
+    SourceItemModel("ABC", true),
+    SourceItemModel("CNN", false),
+    SourceItemModel("Republic", true),
+    SourceItemModel("Aaj Tak", false),
+    SourceItemModel("Zee News", true),
+    SourceItemModel("Nine News", false),
+    SourceItemModel("Aljazeera", true),
+    SourceItemModel("Seven News", false),
+)
+
+@Preview
+@Composable
+fun SourceItemPreview() {
+    SourceItem(SourceItemModel("ABC", true))
 }
 
 @Preview
